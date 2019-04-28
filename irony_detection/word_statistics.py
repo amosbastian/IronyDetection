720919cc7c53406c7db77cf371f899fcd4f39b95
amosbastian <amos_bastian@hotmail.com>
2019-04-28 17:29:41 +0200

    Add word_frequency function

--- irony_detection/word_statistics.py
+++ irony_detection/word_statistics.py
@@ -1,5 +1,24 @@
 import os
+import string
 
+from ekphrasis.classes.preprocessor import TextPreProcessor
+from ekphrasis.classes.tokenizer import SocialTokenizer
+from ekphrasis.dicts.emoticons import emoticons
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+
+text_processor = TextPreProcessor(
+    normalize=[],
+    annotate={},
+    fix_html=True,
+    segmenter="twitter",
+    corrector="twitter",
+    unpack_hashtags=False,
+    unpack_contractions=True,
+    spell_correct_elong=False,
+    tokenizer=SocialTokenizer(lowercase=True).tokenize,
+    dicts=[emoticons]
+)
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -8,6 +27,7 @@ def parse_dataset(training_set):
     training_directory = f"{DIR_PATH}/../datasets/train/"
     labels = []
     corpus = []
+
     with open(f"{training_directory}{training_set}.txt") as dataset:
         for line in dataset.readlines():
             if line.lower().startswith("tweet index"):
@@ -23,5 +43,18 @@ def parse_dataset(training_set):
     return labels, corpus
 
 
+def word_frequency(corpus):
+    words = []
+    for sentence in corpus:
+        # Remove punctuation
+        sentence = sentence.translate(str.maketrans("", "", string.punctuation))
+
+        # Extend words list while removing stop words
+        words.extend([word for word in text_processor.pre_process_doc(sentence)
+                      if word not in stopwords.words("english")])
+
+    print(FreqDist(words).most_common(20))
+
 if __name__ == "__main__":
-    print(parse_dataset("SemEval2018-T3-train-taskA"))
+    labels, corpus = parse_dataset("SemEval2018-T3-train-taskA")
+    word_frequency(corpus)
