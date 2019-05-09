3dd8ca9651b4fbc7437174fbf4f3bc0a063b9289
amosbastian <amos_bastian@hotmail.com>
2019-05-09 16:00:44 +0200

    Add plot generation code using pyplot

--- models/benchmark_system/plot.py
+++ models/benchmark_system/plot.py
@@ -1,20 +1,22 @@
-import os
 import csv
+import os
 
+import matplotlib
+import matplotlib.pyplot as plt
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
 def plot_handler():
     plots = {
-        "emoji_frequency_ironic": "",
-        "emoji_frequency_non_ironic": "",
-        "emoji_frequency_ironic_vs_non_ironic": "",
-        "emoji_frequency": "",
-        "gram_frequency_ironic": "",
-        "gram_frequency_non_ironic": "",
-        "gram_frequency_ironic_vs_non_ironic": "",
-        "gram_frequency": "",
+        "emoji_frequency_ironic_vs_non_ironic": "1-emoji - Ironic vs. Non-ironic",
+        "emoji_frequency_ironic": "1-emoji - Ironic",
+        "emoji_frequency_non_ironic": "1-emoji - Non-ironic",
+        "emoji_frequency": "1-emoji - All",
+        "gram_frequency_ironic_vs_non_ironic": "1-gram - Ironic vs. Non-ironic",
+        "gram_frequency_ironic": "1-gram - Ironic",
+        "gram_frequency_non_ironic": "1-gram - Non-ironic",
+        "gram_frequency": "1-gram - All",
     }
 
     with open(f"{DIR_PATH}/output/output_1.csv") as f:
@@ -35,8 +37,22 @@ def plot_handler():
 
 
 def plot(results, description):
-    for result in sorted(results, key=lambda x: float(x[-1]), reverse=True):
-        print(result)
+    sorted_results = sorted(results, key=lambda x: int(x[0].split("_")[-1][:-4]))
+    x = [int(result[0].split("_")[-1][:-4]) for result in sorted_results]
+    y = [float(result[-1]) for result in sorted_results]
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+
+    if "emoji" in description.lower():
+        xlabel = "$N$ emojis removed"
+    else:
+        xlabel = "$N$ words removed"
+
+    ax.set(xlabel=xlabel, ylabel="$F_1$ score",
+           title=description)
+    ax.grid()
+    matplotlib.pyplot.xticks(x)
+    plt.show()
 
 
 if __name__ == "__main__":
