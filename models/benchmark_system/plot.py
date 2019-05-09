7b7a15ca32537b17d125ba43f038bc8cf79aa332
amosbastian <amos_bastian@hotmail.com>
2019-05-09 20:28:50 +0200

    Rework plot function & add plot_all function
    The plot function has now be changed to plot_one function,
    and the plot_all function has been added, which plots e.g. all
    the 1-gram performances in one figure.

--- models/benchmark_system/plot.py
+++ models/benchmark_system/plot.py
@@ -3,22 +3,24 @@ import os
 
 import matplotlib
 import matplotlib.pyplot as plt
+from textwrap import wrap
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
-def plot_handler():
-    plots = {
-        "emoji_frequency_ironic_vs_non_ironic": "1-emoji - Ironic vs. Non-ironic",
-        "emoji_frequency_ironic": "1-emoji - Ironic",
-        "emoji_frequency_non_ironic": "1-emoji - Non-ironic",
-        "emoji_frequency": "1-emoji - All",
-        "gram_frequency_ironic_vs_non_ironic": "1-gram - Ironic vs. Non-ironic",
-        "gram_frequency_ironic": "1-gram - Ironic",
-        "gram_frequency_non_ironic": "1-gram - Non-ironic",
-        "gram_frequency": "1-gram - All",
+def plot_handler(n):
+    plot_dictionary = {
+        "emoji_frequency_ironic_vs_non_ironic": ["Ironic vs. Non-ironic"],
+        "emoji_frequency_ironic": ["Ironic"],
+        "emoji_frequency_non_ironic": ["Non-ironic"],
+        "emoji_frequency": ["All"],
+        "gram_frequency_ironic_vs_non_ironic": ["Ironic vs. Non-ironic"],
+        "gram_frequency_ironic": ["Ironic"],
+        "gram_frequency_non_ironic": ["Non-ironic"],
+        "gram_frequency": ["All"],
     }
-    output_file = f"{DIR_PATH}/output/output_1.csv"
+
+    output_file = f"{DIR_PATH}/output/output_{n}.csv"
     baseline_results = baseline(output_file)
 
     with open(output_file) as f:
@@ -26,19 +28,22 @@ def plot_handler():
         result_list = list(reader)
 
     already_used = []
-    for word_type, description in plots.items():
+    for plot_type, plot_description in plot_dictionary.items():
         relevant_results = []
         for result in result_list:
-            if word_type not in result[0] or result in already_used:
+            if plot_type not in result[0] or result in already_used:
                 continue
 
             already_used.append(result)
             relevant_results.append(result)
+        plot_dictionary[plot_type].append(relevant_results)
+        plot_one(n, relevant_results, [plot_type, plot_description[0]],
+                 baseline_results)
 
-        plot(relevant_results, [word_type, description], baseline_results)
+    plot_all(n, plot_dictionary, baseline_results)
 
 
-def plot(results, plot_list, baseline_results):
+def plot_one(n, results, plot_list, baseline_results):
     plot_filename, plot_description = plot_list
 
     sorted_results = sorted(results, key=lambda x: int(x[0].split("_")[-1][:-4]))
@@ -48,19 +53,61 @@ def plot(results, plot_list, baseline_results):
     ax.plot(x, y)
 
     if "emoji" in plot_description.lower():
-        xlabel = "$N$ emojis removed"
+        xlabel = f"Number of {n}-emojis removed from tokenised training set"
+        title = (f"$F_1$ scores after removing {n}-emojis"
+                 f"({plot_description.lower()}) from the default (tokenised) "
+                 "training set")
     else:
-        xlabel = "$N$ words removed"
+        xlabel = f"Number of {n}-grams removed from tokenised training set"
+        title = (f"$F_1$ scores after removing {n}-grams"
+                 f"({plot_description.lower()}) from the default (tokenised) "
+                 "training set")
 
-    ax.set(xlabel=xlabel, ylabel="$F_1$ score",
-           title=plot_description)
+    ax.set(xlabel=xlabel, ylabel="$F_1$ score", title="\n".join(wrap(title, 60)))
     ax.grid()
     matplotlib.pyplot.xticks(x)
     ax.plot(x, [baseline_results[0]] * len(x), label="Default")
     ax.plot(x, [baseline_results[1]] * len(x), label="Default (tokenised)")
     plt.legend(loc="best")
-    fig.savefig(f"{DIR_PATH}/figures/{plot_filename}.png")
-    plt.show()
+    fig.savefig(f"{DIR_PATH}/figures/{n}-grams/{n}-{plot_filename}.png")
+
+
+def plot_all(n, plot_dictionary, baseline_results):
+    emoji_plots = [value for key, value in plot_dictionary.items()
+                   if "emoji" in key]
+    ngram_plots = [value for key, value in plot_dictionary.items()
+                   if "gram" in key]
+
+    for i, plots in enumerate([emoji_plots, ngram_plots]):
+        fig, ax = plt.subplots()
+        ax.grid()
+
+        if not i:
+            xlabel = f"Number of {n}-emojis removed from tokenised training set"
+            title = (f"$F_1$ scores after removing {n}-emojis from the default"
+                     " (tokenised) training set")
+            plot_filename = f"{n}-emoji_all"
+        else:
+            xlabel = f"Number of {n}-emojis removed from tokenised training set"
+            title = (f"$F_1$ scores after removing {n}-grams from the default"
+                     " (tokenised) training set")
+            plot_filename = f"{n}-gram_all"
+
+        for plot in plots:
+            results = plot[1]
+            sorted_results = sorted(results, key=lambda x: int(x[0].split("_")[-1][:-4]))
+
+            x = [int(result[0].split("_")[-1][:-4]) for result in sorted_results]
+            y = [float(result[-1]) for result in sorted_results]
+
+            ax.plot(x, y, label=plot[0])
+
+        matplotlib.pyplot.xticks(x)
+        ax.plot(x, [baseline_results[0]] * len(x), label="Default")
+        ax.plot(x, [baseline_results[1]] * len(x), label="Default (tokenised)")
+        plt.legend(loc="best")
+        ax.set(xlabel=xlabel, ylabel="$F_1$ score", title="\n".join(wrap(title, 60)))
+        fig.savefig(f"{DIR_PATH}/figures/{n}-grams/{plot_filename}.png")
 
 
 def baseline(filename):
@@ -80,4 +127,5 @@ def baseline(filename):
     return baseline_results
 
 if __name__ == "__main__":
-    plot_handler()
+    for n in range(1, 5):
+        plot_handler(n)
