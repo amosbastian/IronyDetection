c37af8d7fb41d83dec3b9c10580c77c7aa637b9f
amosbastian <amos_bastian@hotmail.com>
2019-05-16 18:06:44 +0200

    Update frequency_statistics.py to write to file

--- irony_detection/frequency_statistics.py
+++ irony_detection/frequency_statistics.py
@@ -1,6 +1,11 @@
+import csv
 import logging
 import os
 import re
+from textwrap import wrap
+
+import matplotlib
+import matplotlib.pyplot as plt
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 training_directory = f"{DIR_PATH}/../datasets/train/"
@@ -35,14 +40,17 @@ def frequency_handler():
 
     total_words = count_words(default_training[0])
 
+    fout = open(f"{DIR_PATH}/words_removed.csv", "w+")
+    fout.write("Training Set,Number of Words Removed,Percentage of Words Removed\n")
+
     for filename in os.listdir(training_directory):
         if filename in default_training:
             continue
 
         word_count = count_words(filename)
         percentage_removed = 100 - percentage(word_count, total_words)
-        print(f"{filename}: removes {percentage_removed}% ({total_words - word_count})")
-
+        words_removed = total_words - word_count
+        fout.write(f"{filename},{words_removed},{percentage_removed}\n")
 
 if __name__ == "__main__":
     frequency_handler()
