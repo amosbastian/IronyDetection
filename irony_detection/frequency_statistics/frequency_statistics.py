deb1d607f788dafbf89ead4e3514af6b71061932
amosbastian <amos_bastian@hotmail.com>
2019-05-16 19:26:30 +0200

    Add plotting to frequency_statistics.py

--- irony_detection/frequency_statistics/frequency_statistics.py
+++ irony_detection/frequency_statistics/frequency_statistics.py
@@ -67,6 +67,105 @@ def group_training_sets():
     return groups
 
 
+def plot_handler(n):
+    plot_dictionary = {
+        "emoji_frequency_ironic_vs_non_ironic_(ratio)": ["Ironic vs. Non-ironic (ratio)"],
+        "emoji_frequency_ironic_vs_non_ironic": ["Ironic vs. Non-ironic"],
+        "emoji_frequency_ironic": ["Ironic"],
+        "emoji_frequency_non_ironic": ["Non-ironic"],
+        "emoji_frequency": ["All"],
+        "gram_frequency_ironic_vs_non_ironic_(ratio)": ["Ironic vs. Non-ironic (ratio)"],
+        "gram_frequency_ironic_vs_non_ironic": ["Ironic vs. Non-ironic"],
+        "gram_frequency_ironic": ["Ironic"],
+        "gram_frequency_non_ironic": ["Non-ironic"],
+        "gram_frequency": ["All"],
+    }
+
+    output_file = f"{DIR_PATH}/output/words_removed_{n}.csv"
+
+    with open(output_file) as f:
+        reader = csv.reader(f)
+        result_list = list(reader)
+
+    already_used = []
+    for plot_type, plot_description in plot_dictionary.items():
+        relevant_results = []
+        for result in result_list:
+            if plot_type not in result[0] or result in already_used:
+                continue
+
+            already_used.append(result)
+            relevant_results.append(result)
+        plot_dictionary[plot_type].append(relevant_results)
+        plot_one(n, relevant_results, [plot_type, plot_description[0]])
+
+    plot_all(n, plot_dictionary)
+
+
+def plot_one(n, results, plot_list):
+    plot_filename, plot_description = plot_list
+
+    sorted_results = sorted(results, key=lambda x: int(x[0].split("_")[-1][:-4]))
+    x = [int(result[0].split("_")[-1][:-4]) for result in sorted_results]
+    y = [float(result[-1]) for result in sorted_results]
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+
+    if "emoji" in plot_description.lower():
+        xlabel = f"The number of most frequent {n}-emojis removed"
+        title = ("The percentage of words removed from the default "
+                 "(tokenised) training set when removing the $N$ most "
+                 f"frequent ({plot_description.lower()}) {n}-emojis")
+    else:
+        xlabel = f"The number of most frequent {n}-grams removed"
+        title = ("The percentage of words removed from the default "
+                 "(tokenised) training set when removing the $N$ most "
+                 f"frequent ({plot_description.lower()}) {n}-grams")
+
+    ax.set(xlabel=xlabel, ylabel="% words removed", title="\n".join(wrap(title, 60)))
+    ax.grid()
+    matplotlib.pyplot.xticks(x)
+    fig.savefig(f"{DIR_PATH}/figures/{n}-grams/{n}-{plot_filename}.png")
+
+
+def plot_all(n, plot_dictionary):
+    emoji_plots = [value for key, value in plot_dictionary.items()
+                   if "emoji" in key]
+    ngram_plots = [value for key, value in plot_dictionary.items()
+                   if "gram" in key]
+
+    for i, plots in enumerate([emoji_plots, ngram_plots]):
+        fig, ax = plt.subplots()
+        ax.grid()
+
+        if not i:
+            xlabel = f"The number of most frequent {n}-emojis removed"
+            title = ("The percentage of words removed from the default "
+                     "(tokenised) training set when removing the $N$ most "
+                     f"frequent {n}-emojis")
+            plot_filename = f"{n}-emoji_all"
+        else:            
+            xlabel = f"The number of most frequent {n}-grams removed"
+            title = ("The percentage of words removed from the default "
+                     "(tokenised) training set when removing the $N$ most "
+                     f"frequent {n}-grams")
+            plot_filename = f"{n}-gram_all"
+
+        for plot in plots:
+            results = plot[1]
+            sorted_results = sorted(results, key=lambda x: int(x[0].split("_")[-1][:-4]))
+
+            x = [int(result[0].split("_")[-1][:-4]) for result in sorted_results]
+            y = [float(result[-1]) for result in sorted_results]
+
+            ax.plot(x, y, label=plot[0])
+
+        matplotlib.pyplot.xticks(x)
+        plt.legend(loc="best")
+        ax.set(xlabel=xlabel, ylabel="% words removed", title="\n".join(wrap(title, 60)))
+        fig.savefig(f"{DIR_PATH}/figures/{n}-grams/{plot_filename}.png")
+
+
 def frequency_handler():
     default = ["SemEval2018-T3-train-taskA_emoji_tokenised.txt",
                "SemEval2018-T3-train-taskA_emoji.txt",
@@ -89,4 +188,6 @@ def frequency_handler():
             fout.write(f"{filename},{words_removed},{percentage_removed}\n")
 
 if __name__ == "__main__":
-    frequency_handler()
+    # frequency_handler()
+    for n in range(1, 5):
+        plot_handler(n)
